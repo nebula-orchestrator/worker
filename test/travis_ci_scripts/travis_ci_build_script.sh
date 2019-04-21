9100abc655e87e32ec84566c3d2d4d928ed58a49
Naor Livne <naor@mintigo.com>
2019-04-21 14:25:14 +0300

    comment syntex fix

--- test/travis_ci_scripts/travis_ci_build_script.sh
+++ test/travis_ci_scripts/travis_ci_build_script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# run the following to start the API, MongoDB
+# run the following to start the API & MongoDB
 sudo docker-compose -f test/travis_ci_scripts/docker-compose.yml pull
 sudo docker-compose -f test/travis_ci_scripts/docker-compose.yml up -d
 

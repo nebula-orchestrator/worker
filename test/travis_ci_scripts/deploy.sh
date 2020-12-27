bf7885b72831cad7c2baea23694b0ad64e313d46
Naor Livne <naorlivne@gmail.com>
2020-12-27 11:32:51 +0200

    Update deploy.sh

--- test/travis_ci_scripts/deploy.sh
+++ test/travis_ci_scripts/deploy.sh
@@ -1,5 +1,9 @@
 #!/usr/bin/env bash
 
+sudo chmod 777 /home/travis/.docker/config.json
+echo "$DOCKER_HUB_PASSWORD" | docker login -u "$DOCKER_HUB_USER" --password-stdin
+sudo chmod 777 /home/travis/.docker/config.json
+
 if [ "$TRAVIS_BRANCH" == "master" ] && [ "$TRAVIS_PULL_REQUEST" == false ]; then
     docker build -t $DOCKER_HUB_ORG/$DOCKER_HUB_REPO .
     docker push $DOCKER_HUB_ORG/$DOCKER_HUB_REPO

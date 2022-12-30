731b6e78164e00183ea593c49eb07c66f380102a
paritoshpr <pramanan3@MacBook-Air-4.local>
2022-12-29 21:52:17 -0800

    added container id name mapping push to redis

--- functions/reporting/redis.py
+++ functions/reporting/redis.py
@@ -20,3 +20,28 @@ class RedisConnection:
         except Exception as e:
             print(e, file=sys.stderr)
             print("Report delivery to redis failed")
+
+    def map_container_id(self,report):
+        container_info = report["apps_containers"]
+        for container in container_info:
+
+            try:
+                container_id = container["id"]
+                container_name = container["name"].split("-")[0].replace("/","")
+                container_time = container["read"]
+            except Exception as e:
+                print(e, file=sys.stderr)
+                print("Error reporting containers on host {}".format(report["hostname"]))
+                continue
+
+            try:
+                container_map = {}
+                container_map["name"] = container_name
+                container_map["time"] = container_time
+
+                #keys can be signed here with timestamp for extra protection
+                self.redisObj.set("container_"+container_id, pickle.dumps(container_map),)
+
+            except Exception as e:
+                print(e, file=sys.stderr)
+                print("Container Map push to redis failed for {}".format(container_name))
--- worker.py
+++ worker.py
@@ -521,6 +521,7 @@ if __name__ == "__main__":
                     if monotonic_id_increase is True or report_on_update_only is False:
                         report = reporting_object.current_status_report(local_device_group_info, monotonic_id_increase)
                         redis_connection.push_report(report)
+                        redis_connection.map_container_id(report)
                 except Exception as e:
                     print(e, file=sys.stderr)
                     if reporting_fail_hard is False:
